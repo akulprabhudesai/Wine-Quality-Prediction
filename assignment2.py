d910b07c6bc42c8696535cae98671886bdbbeba7
akulprabhudesai <64514123+akulprabhudesai@users.noreply.github.com>
2020-05-06 09:49:29 -0400

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,18 +1,16 @@
 from pyspark.sql import DataFrame
 from pyspark import SparkContext, SQLContext
-from pyspark.ml import Pipeline
 from pyspark.ml.feature import StringIndexer, VectorIndexer
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import Imputer
 from pyspark.sql.functions import when
 from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.feature import StandardScaler
 from pyspark.mllib.util import MLUtils
 from pyspark.mllib.evaluation import MulticlassMetrics
 from pyspark.ml.classification import RandomForestClassifier
-from pyspark.mllib.evaluation import MulticlassMetrics
+
 
 
 spark = SparkSession.builder.master("local").appName("assignment2").config("spark.some.config.option","some-value").getOrCreate()
